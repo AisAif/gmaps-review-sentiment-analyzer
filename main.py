bb00ae6705a01264f6e961b2bb0eb695ae5d11b4
luqmanadi <78971486+luqmanadi@users.noreply.github.com>
2024-03-13 15:56:48 +0700

    File Scrapping menggunakan Serpapi

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from services.scrap import run
 
 if __name__ == '__main__':
-    
+
     # Scraping Data From Gmaps
-    url = 'https://www.google.com/maps/place/Pasar+Jetak/@-6.784863,111.3781444,13.89z/data=!4m16!1m9!3m8!1s0x2e773c81d9ccb39b:0xecbce8a5501796d1!2sPasar+Sulang!8m2!3d-6.8073457!4d111.3799229!9m1!1b1!16s%2Fg%2F1hm4wbq92!3m5!1s0x2e773cd455970d05:0xf0324592960bba11!8m2!3d-6.7704319!4d111.4241411!16s%2Fg%2F11cmmschm7?entry=ttu'
+    url = "https://www.google.com/maps/place/Air+Terjun+Grojogan+Sewu/@-7.6608394,110.8259177,11z/data=!4m13!1m3!2m2!1swisata+jawa+tengah!6e1!3m8!1s0x2e798a3aaef24383:0x3d3da6236af2202a!8m2!3d-7.6608394!4d111.1307883!9m1!1b1!15sChJ3aXNhdGEgamF3YSB0ZW5nYWhaFCISd2lzYXRhIGphd2EgdGVuZ2FokgESdG91cmlzdF9hdHRyYWN0aW9umgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJYZUhSeGFGOUJSUkFC4AEA!16s%2Fm%2F056n6r1?entry=ttu"
     run(url=url)
